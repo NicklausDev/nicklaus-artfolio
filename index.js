a2c50d77b56c014e2a0bc5c75014717a92ddd72f
Nicklaus Wandera <118543212+NicklausDev@users.noreply.github.com>
2024-04-16 04:56:24 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const  isMobile = window.matchMedia('(max-width: 768px)').matches;
+
+const initLenis = () => {
+    const lenis = new Lenis({
+        content: document.querySelector('.frame'),
+
+        lerp: 0.05,
+        smoothWheel: true,
+        orientation: isMobile ? 'vertical' : 'horizontal',
+    });
+
+    function raf(time) {
+        lenis.raf(time);
+        requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+};
+
+initLenis();
